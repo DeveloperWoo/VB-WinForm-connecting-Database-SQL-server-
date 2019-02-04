c4dbe1850939e139dba55e12084a68e51ab9021c
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-02-04 00:14:32 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtJobTitle = New System.Windows.Forms.TextBox()
+        Me.txtJobID = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtMinSal = New System.Windows.Forms.TextBox()
+        Me.txtMaxSal = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.txtMaxSal)
+        Me.GroupBox1.Controls.Add(Me.txtMinSal)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtJobTitle)
+        Me.GroupBox1.Controls.Add(Me.txtJobID)
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(23, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(805, 221)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Jobs"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(296, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(115, 22)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Minimum Salary"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(344, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 22)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Job Title"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(357, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 22)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Job ID"
+        '
+        'txtJobTitle
+        '
+        Me.txtJobTitle.Location = New System.Drawing.Point(418, 84)
+        Me.txtJobTitle.Name = "txtJobTitle"
+        Me.txtJobTitle.Size = New System.Drawing.Size(350, 27)
+        Me.txtJobTitle.TabIndex = 2
+        '
+        'txtJobID
+        '
+        Me.txtJobID.Location = New System.Drawing.Point(418, 48)
+        Me.txtJobID.Name = "txtJobID"
+        Me.txtJobID.Size = New System.Drawing.Size(350, 27)
+        Me.txtJobID.TabIndex = 1
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 22
+        Me.ListBox1.Location = New System.Drawing.Point(18, 65)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(236, 136)
+        Me.ListBox1.TabIndex = 0
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(23, 281)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 27
+        Me.DataGridView1.Size = New System.Drawing.Size(805, 236)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(296, 158)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(119, 22)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Maximum Salary"
+        '
+        'txtMinSal
+        '
+        Me.txtMinSal.Location = New System.Drawing.Point(417, 121)
+        Me.txtMinSal.Name = "txtMinSal"
+        Me.txtMinSal.Size = New System.Drawing.Size(350, 27)
+        Me.txtMinSal.TabIndex = 8
+        '
+        'txtMaxSal
+        '
+        Me.txtMaxSal.Location = New System.Drawing.Point(417, 159)
+        Me.txtMaxSal.Name = "txtMaxSal"
+        Me.txtMaxSal.Size = New System.Drawing.Size(350, 27)
+        Me.txtMaxSal.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(24, 40)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 22)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Job List"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(37, 256)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(81, 22)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Employees"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 22.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(848, 529)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Font = New System.Drawing.Font("Arial Narrow", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Master VB WinForm & SQL server"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtJobTitle As TextBox
+    Friend WithEvents txtJobID As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents txtMaxSal As TextBox
+    Friend WithEvents txtMinSal As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        connectionDB.Open()
+        query = "SELECT * FROM dbo.jobs"
+        sqlCommand = New SqlCommand(query, connectionDB)
+        dataAdapter = New SqlDataAdapter(sqlCommand)
+        dataSet = New DataSet
+        dataAdapter.Fill(dataSet, "Job")
+
+        ListBox1.DataSource = dataSet.Tables("Job")
+        ListBox1.DisplayMember = "job_id"
+
+        bindingSource = New BindingSource
+        bindingSource.DataSource = dataSet.Tables("Job")
+
+        txtJobID.DataBindings.Add("Text", bindingSource, "job_id")
+        txtJobTitle.DataBindings.Add("Text", bindingSource, "job_title")
+        txtMinSal.DataBindings.Add("Text", bindingSource, "min_salary")
+        txtMaxSal.DataBindings.Add("Text", bindingSource, "max_salary")
+
+        ListBox1.SelectedIndex = 0
+        DisplayEmployees()
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        If Not IsNothing(bindingSource) Then
+            bindingSource.Position = ListBox1.SelectedIndex
+        End If
+        DisplayEmployees()
+    End Sub
+
+    Private Sub DisplayEmployees()
+        If Not IsNothing(dataSet.Tables("Employees")) Then
+            dataSet.Tables("Employees").Clear()
+        End If
+        query = "SELECT job_id, employee_id, first_name, last_name, hire_date, salary " &
+                "FROM employees " &
+                "WHERE job_id = '" & txtJobID.Text & "'"
+
+        sqlCommand.CommandText = query
+        dataAdapter.SelectCommand = sqlCommand
+        dataAdapter.Fill(dataSet, "Employees")
+
+        DataGridView1.DataSource = dataSet.Tables("Employees")
+    End Sub
+
+End Class
